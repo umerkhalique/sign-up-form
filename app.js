287333285ae01fd39981b8579f3556ce3b91d671
umer khalique <omerkhalique13@gmail.com>
2022-02-26 19:21:58 +0500

    update -lastname form validation added

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ submit.addEventListener("submit", function(e){
     if (document.getElementById('firstName').value == 0 || document.getElementById('firstName').value == ""){
         alert("please enter a valid first name");
     };
+    if (document.getElementById('lastName').value == 0 || document.getElementById('lastName').value == ""){
+        alert("please enter a valid last name");
+    };
 });
